5a1e966c00064439b2926ce606d118a2da52c79e
Benjamin Lambert - GLC <benjamin@glconception.com>
2019-11-18 14:17:28 +0100

    aded Document Entity

--- NOTES_DEV.md
+++ NOTES_DEV.md
@@ -1,14 +1,36 @@
 ### installer les modules PHP symfony
 
+```bash
 composer install
+```
 
 ### installer les dépendances symfony
 selon si npm ou yarn installé
 
+```bash
 yarn install
+```
+
+```bash
 npm install
+```
 
 ### processer les js, css etc
 
-webpack encore
+```bash
+webpack encore --watch
+```
 
+###générer les entités
+https://symfony.com/doc/current/doctrine/reverse_engineering.html
+
+```bash
+php bin/console doctrine:mapping:import "App\Entity" annotation --path=src/Entity
+```
+
+
+ajouter les get/set
+
+```bash
+php bin/console make:entity --regenerate App
+```
---
+++ src/Entity/Document.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Documents
+ *
+ * @ORM\Table(name="documents")
+ * @ORM\Entity
+ */
+class Document
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="source", type="text", nullable=false)
+     */
+    private $source;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text", nullable=false)
+     */
+    private $content;
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(name="length", type="integer", nullable=true)
+     */
+    private $length;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSource(): ?string
+    {
+        return $this->source;
+    }
+
+    public function setSource(string $source): self
+    {
+        $this->source = $source;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getLength(): ?int
+    {
+        return $this->length;
+    }
+
+    public function setLength(?int $length): self
+    {
+        $this->length = $length;
+
+        return $this;
+    }
+
+
+}
